4d1a15929a3eca58fcb86bae33a3156e81d9308e
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-07-29 22:03:01 +0300

    defined list in candidate controller

--- grails-app/controllers/skilloo/CandidateController.groovy
+++ grails-app/controllers/skilloo/CandidateController.groovy
@@ -7,4 +7,9 @@ class CandidateController {
     def index() {
         redirect(action: "list")
     }
+
+	def list() {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		[CandidateList: Candidate.list(params), CandidateTotal: Candidate.count()]
+	}
 }
