6a0ee847fa429a6b1e8fcc191881dc90fd9a7316
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-08-03 17:39:23 +0300

    added save method

--- grails-app/controllers/skilloo/AddressController.groovy
+++ grails-app/controllers/skilloo/AddressController.groovy
@@ -7,4 +7,15 @@ class AddressController {
     def index() {
         redirect(action: "list")
     }
+	
+    def save() {
+        def address = new Address(params)
+        if (!address.save(flush: true)) {
+            render(view: "create", model: [address: address])
+            return
+        }
+
+		flash.message = message(code: 'default.created.message', args: [message(code: 'address.label', default: 'Address'), address.id])
+        redirect(action: "show", id: address.id)
+    }
 }
