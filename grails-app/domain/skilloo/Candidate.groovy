bf3680a7c53edf38ed50cb86c63429d41c28e50e
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-08-03 17:39:42 +0300

    changed nullable fields

--- grails-app/domain/skilloo/Candidate.groovy
+++ grails-app/domain/skilloo/Candidate.groovy
@@ -20,26 +20,23 @@ class Candidate {
     static hasMany = [candidateQualifications:CandidateQualification, candidateNotes:CandidateNote, payrolls:Payroll, jobs:Job, placements:Placement ]
 
     static constraints = {   
-        consultant(nullable: true)
+        consultant nullable: true
 		
 		/**
 		 * temporary added in order to allow various tests on candidates list
 		 */
-		birthDate(nullable: true)
-		driver(nullable: true)
-		sponsored(nullable: true)
-		carOwner(nullable: true)
-		active(nullable: true)
-		telephoneNumber(nullable: true)
-		email(nullable: true)
-		address(nullable: true)
-		mainTrade(nullable: true)
-		candidateNotes(nullable: true)
-		payrolls(nullable: true)
-		candidateQualifications(nullable: true)
-		jobs(nullable: true)
-		placements(nullable: true)
+		birthDate nullable: true
+		driver nullable: true
+		sponsored nullable: true
+		carOwner nullable: true
+		active nullable: true
+		email nullable: true
+		mainTrade nullable: true
+		candidateNotes nullable: true
+		payrolls nullable: true
+		candidateQualifications nullable: true
+		jobs nullable: true
+		placements nullable: true
 		
-        email email:true 
     }
 }
