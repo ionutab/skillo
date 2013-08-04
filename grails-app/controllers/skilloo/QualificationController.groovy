eae9ba6478ddec2507fe215df5c804e2ee1511ac
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-08-04 17:12:44 +0300

    added listModal action in order to be able to create a separate listing for the modal page

--- grails-app/controllers/skilloo/QualificationController.groovy
+++ grails-app/controllers/skilloo/QualificationController.groovy
@@ -7,4 +7,13 @@ class QualificationController {
     def index() {
         redirect(action: "list")
     }
+
+    def listModal() {
+        println "Qualification list modal"
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+
+        def qualificationList = Qualification.createCriteria().list(params) {}
+
+        render(view: "modalList", model: [QualificationList: qualificationList, QualificationTotal: Qualification.count()])
+    }
 }
