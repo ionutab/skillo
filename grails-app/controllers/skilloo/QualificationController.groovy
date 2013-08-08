486c133292c3bc59a1c6fa94b3e48c5d25405620
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-08-08 09:18:26 +0300

    removed action no longer required

--- grails-app/controllers/skilloo/QualificationController.groovy
+++ grails-app/controllers/skilloo/QualificationController.groovy
@@ -7,13 +7,4 @@ class QualificationController {
     def index() {
         redirect(action: "list")
     }
-
-    def listModal() {
-        println "Qualification list modal"
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-
-        def qualificationList = Qualification.createCriteria().list(params) {}
-
-        render(view: "modalList", model: [QualificationList: qualificationList, QualificationTotal: Qualification.count()])
-    }
 }
