4c7d31d57a7e29f94b70e9499482401d5601d8c8
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-08-22 22:26:46 +0300

    added Consultant information

--- grails-app/domain/skilloo/Consultant.groovy
+++ grails-app/domain/skilloo/Consultant.groovy
@@ -2,10 +2,20 @@ package skilloo
 
 class Consultant {
 
-    String extraInformation
+    String firstName
+    String lastName
+    String email
+    String information
+
+    User user
 
     static hasMany = [candidateNotes:CandidateNote, clientComments:ClientComment]
     
     static constraints = {
+        email email: true
+
+        candidateNotes nullable: true
+        clientComments nullable: true
+        information nullable: true
     }
 }
