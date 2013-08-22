15130b72e85989bbe8b94727f65f45cfc2d7838f
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-08-22 22:25:45 +0300

    added popovers

--- web-app/js/candidates-list.js
+++ web-app/js/candidates-list.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
+
     $("#add-candidates-tab-button").click(function() {
         alert('duplicating');
     });
+
+    $('html').click(function(e) {
+        $('.popable').popover('hide');
+    });
+
+    $('.popable').popover({
+        html : true,
+        trigger: 'manual',
+        content: function() {
+          var $input = $( this );
+          return $('#popover_content_wrapper_' + $input.attr( "id" )).html();
+        }
+    }).click(function(e) {
+        $(this).popover('toggle');
+        e.stopPropagation();
+    });
+
 });
 
 
