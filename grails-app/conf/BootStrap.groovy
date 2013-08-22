d4bd0a022d0215bfe99c4bdc6ca9a09cccc0c291
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-08-22 22:23:16 +0300

    creating a default Consultant to be used for testing purposes for a while now

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,3 +1,4 @@
+import skilloo.Consultant
 import skilloo.Role
 import skilloo.User
 import skilloo.UserRole
@@ -7,9 +8,12 @@ class BootStrap {
     def springSecurityService
 
     def init = { servletContext ->
+
         if (!User.count()) {
             def role = new Role(authority: 'ROLE_USER').save(flush: true)
-            def user = new User(username: 'ditech', enabled: true, password:'ditech').save(flush: true)
+            def user = new User(username: 'skillo', enabled: true, password: 'skillo').save(flush: true)
+            new Consultant(firstName: 'John', lastName: 'Shepard', email: 'bujdeialexandru@gmail.com', user: user ).save(flush: true)
+
             UserRole.create user, role, true
 
             assert User.count() == 1
