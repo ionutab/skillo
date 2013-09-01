a79370c2d96798f473605e56c7f57fa025da185f
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-09-02 00:41:14 +0300

    the 'forms' resource pack

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -10,21 +10,27 @@ modules = {
 	}
 	
 	maps {
-		dependsOn 'bootstrap'
 		resource url:'css/maps/googlemaps.css'
 		resource url:'js/maps/googlemaps.js'
 	}
 	
 	candidates {
-		dependsOn 'bootstrap'
 		resource url:'js/candidates-list.js'
 		resource url:'js/candidates-form.js'
 	}
 
 	select2 {
-		dependsOn 'bootstrap'
         resource url:'js/select2/select2.js'
         resource url:'css/select2/select2.css'
         resource url:'css/select2/select2-bootstrap.css'
 	}
+
+    datepicker {
+        resource url:'js/bootstrap-datepicker.js'
+        resource url:'css/datepicker.css'
+    }
+
+    forms {
+        dependsOn 'select2,datepicker'
+    }
 }
