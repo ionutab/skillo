88c4644d945341b57fe31fbd42a83262754f225a
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-10-13 23:30:47 +0300

    removed unnecessary save ovverride

--- grails-app/controllers/skilloo/AddressController.groovy
+++ grails-app/controllers/skilloo/AddressController.groovy
@@ -8,14 +8,4 @@ class AddressController {
         redirect(action: "list")
     }
 	
-    def save() {
-        def address = new Address(params)
-        if (!address.save(flush: true)) {
-            render(view: "create", model: [address: address])
-            return
-        }
-
-		flash.message = message(code: 'default.created.message', args: [message(code: 'address.label', default: 'Address'), address.id])
-        redirect(action: "show", id: address.id)
-    }
 }
