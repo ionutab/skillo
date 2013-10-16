b2a2c9d75e649011facb965dbe6e01ea74cd8468
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-10-17 01:59:00 +0300

    temporarily removed this

--- web-app/js/candidates-form.js
+++ web-app/js/candidates-form.js
@@ -2,11 +2,6 @@ $(document).ready(function() {
 
     $(".date").addClass("input-small");
 
-    $("#mainTradeId").select2({
-            placeholder: "Select a Qualification",
-            allowClear:true
-        }
-    );
     $("#newQualificationId").select2({
             placeholder: "Select a Qualification",
             allowClear:true
