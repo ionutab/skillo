e0e6c03275db78260d5685ea5cccb4f35e94c490
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-11-11 20:32:15 +0200

    updated package reference

---
+++ grails-app/domain/skillo/Address.groovy
@@ -0,0 +1,14 @@
+package skillo
+
+class Address {
+
+    String details
+    Boolean active = Boolean.TRUE
+    PostCode postCode
+
+    static belongsTo = [Candidate,WorkSite]
+
+    static constraints = {
+        details blank: false
+    }
+}
