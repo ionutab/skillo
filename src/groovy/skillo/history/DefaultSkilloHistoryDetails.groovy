08a36dbbb8ab74fb6cb09f69d16c76da2946dafe
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-11-18 02:16:19 +0200

    updated gsp to match new grails plugin details

---
+++ src/groovy/skillo/history/DefaultSkilloHistoryDetails.groovy
@@ -0,0 +1,16 @@
+package skillo.history
+
+import skillo.Consultant
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ionutab
+ * Date: 11/17/13
+ * Time: 4:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class DefaultSkilloHistoryDetails {
+
+    String details
+
+}
