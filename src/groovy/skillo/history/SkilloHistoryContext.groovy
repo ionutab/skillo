9023374e0e7444082f3925502f5aa74e69a56ef1
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2013-11-19 01:59:21 +0200

    added better insert message; will be parametrised

--- src/groovy/skillo/history/SkilloHistoryContext.groovy
+++ src/groovy/skillo/history/SkilloHistoryContext.groovy
@@ -13,8 +13,7 @@ public class SkilloHistoryContext {
 
     void insertHistory(DomainModelEvent insertEvent){
 
-//        insertEvent.history.details = event.getConsultant().getFirstName() + " " + event.getConsultant().getLastName() + " inserted " + event.getClass().getName()
-        insertEvent.setEventDetails("inserted " + insertEvent.getClass().getName())
+        insertEvent.setEventDetails( " added by " + insertEvent.getConsultant().getFirstName() + " " + insertEvent.getConsultant().getLastName() )
 
         if(DomainClassArtefactHandler.isDomainClass(insertEvent.getClass())){
             insertEvent.save()
