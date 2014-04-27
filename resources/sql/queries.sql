c7639eeb3b072473e3310ac29468bd89eb25496b
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-04-27 15:50:44 +0300

    removed unused lines

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,5 +1,3 @@
-select * from post_code;
-select * from qualification;
 
 
 INSERT INTO qualification (code, name, description, can_be_main_trade, version)
@@ -32,8 +30,6 @@ INSERT INTO post_code (version, code, latitude, longitude, easting, northing, gr
     country_code
   FROM post_code_temp;
 
-
-
 update post_code set grind_ref = null where length(grind_ref) = 0 and grind_ref is not null;
 update post_code set district = null where length(district) = 0 and district is not null;
 update post_code set ward = null where length(ward) = 0 and ward is not null;
@@ -44,15 +40,7 @@ update post_code set country = null where length(country) = 0 and country is not
 update post_code set country_code = null where length(country_code) = 0 and country_code is not null;
 
 
-select * from payroll;
-
-
 insert into payroll_company(name,version) values('ICM',0);
 insert into payroll_company(name,version) values('GUILD',0);
-select * from post_code;
 
 
-
-
-
-Select * from post_code where code like 'AB%';
