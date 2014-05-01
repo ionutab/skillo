c532fe978ba893c5e05c9662081adab7e3edc3ff
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-05-01 17:04:44 +0300

    init service tests

---
+++ test/unit/skillo/InitServiceSpec.groovy
@@ -0,0 +1,15 @@
+package skillo
+
+
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(InitService)
+class InitServiceSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
