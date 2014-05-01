d3e9bb68d83aade2b57108f36664305fbcd36c64
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-05-01 17:05:44 +0300

    made one line queries so I can run them by executing scripts in grails

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,35 +1,5 @@
-
-
-INSERT INTO qualification (code, name, description, can_be_main_trade, version)
-  SELECT
-    code,
-    name,
-    description,
-    1 as can_be_main_trade,
-    0 AS version
-  FROM qualification_temp;
-
-update qualification set code = null where length(code) = 0 and code is not null;
-update qualification set description = null where length(description) = 0 and description is not null;
-
-INSERT INTO post_code (version, code, latitude, longitude, easting, northing, grind_ref, county, district, ward, district_code, ward_code, country, country_code)
-  SELECT
-    0 AS version,
-    postcode,
-    latitude,
-    longitude,
-    easting,
-    northing,
-    grind_ref,
-    county,
-    district,
-    ward,
-    district_code,
-    ward_code,
-    country,
-    country_code
-  FROM post_code_temp;
-
+INSERT INTO qualification (code, name, description, can_be_main_trade, version) SELECT code, name, description, 1 as can_be_main_trade, 0 AS version FROM qualification_temp;
+INSERT INTO post_code (version, code, latitude, longitude, easting, northing, grind_ref, county, district, ward, district_code, ward_code, country, country_code) SELECT   0 AS version, postcode, latitude, longitude, easting, northing, grind_ref, county, district, ward, district_code, ward_code, country,country_code FROM post_code_temp;
 update post_code set grind_ref = null where length(grind_ref) = 0 and grind_ref is not null;
 update post_code set district = null where length(district) = 0 and district is not null;
 update post_code set ward = null where length(ward) = 0 and ward is not null;
@@ -38,9 +8,5 @@ update post_code set ward_code = null where length(ward_code) = 0 and ward_code 
 update post_code set county = null where length(county) = 0 and county is not null;
 update post_code set country = null where length(country) = 0 and country is not null;
 update post_code set country_code = null where length(country_code) = 0 and country_code is not null;
-
-
 insert into payroll_company(name,version) values('ICM',0);
 insert into payroll_company(name,version) values('GUILD',0);
-
-
