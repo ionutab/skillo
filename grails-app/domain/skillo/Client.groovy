3b4cc8dcaa442e1f092f973acfc54bbbe2c2eccf
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-05-07 01:51:23 +0300

    more Client workout

--- grails-app/domain/skillo/Client.groovy
+++ grails-app/domain/skillo/Client.groovy
@@ -1,14 +1,42 @@
 package skillo
 
-class Client {
+import skillo.history.SkilloDomainModelWithHistory
+
+class Client implements SkilloDomainModelWithHistory {
 
     String name
     String registrationNumber
+    Address address
+    Boolean active = Boolean.TRUE
 
-    static hasMany = [contacts: Contact, workSite: WorkSite, placement:Placement]
+    static hasMany = [contacts: Contact, workSite: WorkSite, placement:Placement, clientHistory:ClientEvent]
 
     static constraints = {
         name unique: true, blank: false
         registrationNumber unique: true, blank: false
+        address nullable: true
+    }
+
+    @Override
+    void addInsertEvent() {
+    }
+
+    @Override
+    void addInsertEvent(Consultant consultant) {
+        def insertEvent = new ClientEvent()
+        insertEvent.client = this
+
+        insertEvent.consultant = consultant
+
+        skilloHistoryContext.insertHistory(insertEvent)
+    }
+
+    @Override
+    void addUpdateEvent(Consultant consultant) {
+    }
+
+    @Override
+    void addUpdateEvent() {
+
     }
 }
