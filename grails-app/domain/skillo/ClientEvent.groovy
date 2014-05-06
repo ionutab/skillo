5498faefe504ee26d84a0b73dc7db6ab10f77d36
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-05-07 01:51:59 +0300

    clientevent - first version

---
+++ grails-app/domain/skillo/ClientEvent.groovy
@@ -0,0 +1,27 @@
+package skillo
+
+import skillo.history.DefaultHistoryEvent
+import skillo.history.DomainModelEvent
+
+class ClientEvent implements DomainModelEvent {
+
+    Consultant consultant
+    Client client
+    DefaultHistoryEvent eventDetails = new DefaultHistoryEvent()
+
+    static embedded = ['eventDetails']
+
+    static constraints = {
+
+    }
+
+    @Override
+    void setEventDetails(String eventDetails) {
+        this.eventDetails.setDetails(eventDetails)
+    }
+
+    @Override
+    void setEventDetails(DefaultHistoryEvent eventDetails) {
+        this.eventDetails = eventDetails
+    }
+}
