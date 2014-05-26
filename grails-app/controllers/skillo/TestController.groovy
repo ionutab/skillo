19391ab2b25d571ca388d4cc9284ae0aaca67cf2
abujdei <bujdeialexandru@gmail.com>
2014-05-26 22:31:05 +0300

    added a test controller - trying to create some kind of test case

---
+++ grails-app/controllers/skillo/TestController.groovy
@@ -0,0 +1,19 @@
+package skillo
+
+/**
+ * TestController
+ * A controller class handles incoming web requests and performs actions such as redirects, rendering views and so on.
+ */
+class TestController extends BaseController {
+
+    static scaffold = true
+
+    def clientService
+
+    def testSearchClients(){
+        log.info("TC:Search Clients")
+        Client c = new Client()
+
+        clientService.search(c)
+    }
+}
---
+++ test/unit/skillo/TestControllerSpec.groovy
@@ -0,0 +1,17 @@
+package skillo
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(TestController)
+class TestControllerSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
