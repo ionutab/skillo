518dba386f4b3cf2e01454b9b623dc0189912382
Andrei Ene <andrei.ene@mambu.com>
2014-06-23 20:16:59 +0300

    added document domain model, controller, and service

---
+++ test/unit/skillo/DocumentServiceSpec.groovy
@@ -0,0 +1,17 @@
+package skillo
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(DocumentService)
+class DocumentServiceSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
