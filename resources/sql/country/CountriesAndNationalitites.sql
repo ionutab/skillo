5e6a9b3368cf2d972e997341c78197e8051412ca
abujdei <bujdeialexandru@gmail.com>
2014-08-22 10:58:55 +0300

    removed white lines

--- resources/sql/country/CountriesAndNationalitites.sql
+++ resources/sql/country/CountriesAndNationalitites.sql
@@ -1,383 +1,192 @@
 INSERT INTO Country(name, nationality) VALUES ('Afghanistan','Afghan');
-
 INSERT INTO Country(name, nationality) VALUES ('Albania','Albanian');
-
 INSERT INTO Country(name, nationality) VALUES ('Algeria','Algerian');
-
 INSERT INTO Country(name, nationality) VALUES ('Andorra','Andorran');
-
 INSERT INTO Country(name, nationality) VALUES ('Angola','Angolan');
-
 INSERT INTO Country(name, nationality) VALUES ('Antigua and Barbuda','Antiguans, Barbudans');
-
 INSERT INTO Country(name, nationality) VALUES ('Argentina','Argentinean');
-
 INSERT INTO Country(name, nationality) VALUES ('Armenia','Armenian');
-
 INSERT INTO Country(name, nationality) VALUES ('Australia','Australian');
-
-
 INSERT INTO Country(name, nationality) VALUES ('Austria','Austrian');
-
 INSERT INTO Country(name, nationality) VALUES ('Azerbaijan','Azerbaijani');
-
 INSERT INTO Country(name, nationality) VALUES ('The Bahamas','Bahamian');
-
 INSERT INTO Country(name, nationality) VALUES ('Bahrain','Bahraini');
-
 INSERT INTO Country(name, nationality) VALUES ('Bangladesh','Bangladeshi');
-
 INSERT INTO Country(name, nationality) VALUES ('Barbados','Barbadian');
-
 INSERT INTO Country(name, nationality) VALUES ('Belarus','Belarusian');
-
 INSERT INTO Country(name, nationality) VALUES ('Belgium','Belgian');
-
 INSERT INTO Country(name, nationality) VALUES ('Belize','Belizean');
-
 INSERT INTO Country(name, nationality) VALUES ('Benin','Beninese');
-
 INSERT INTO Country(name, nationality) VALUES ('Bhutan','Bhutanese');
-
 INSERT INTO Country(name, nationality) VALUES ('Bolivia','Bolivian');
-
 INSERT INTO Country(name, nationality) VALUES ('Bosnia and Herzegovina','Bosnian, Herzegovinian');
-
 INSERT INTO Country(name, nationality) VALUES ('Botswana','Motswana');
-
 INSERT INTO Country(name, nationality) VALUES ('Brazil','Brazilian');
-
 INSERT INTO Country(name, nationality) VALUES ('Brunei','Bruneian');
-
 INSERT INTO Country(name, nationality) VALUES ('Bulgaria','Bulgarian');
-
 INSERT INTO Country(name, nationality) VALUES ('Burkina Faso','Burkinabe');
-
 INSERT INTO Country(name, nationality) VALUES ('Burundi','Burundian');
-
 INSERT INTO Country(name, nationality) VALUES ('Cambodia','Cambodian');
-
 INSERT INTO Country(name, nationality) VALUES ('Cameroon','Cameroonian');
-
 INSERT INTO Country(name, nationality) VALUES ('Canada','Canadian');
-
 INSERT INTO Country(name, nationality) VALUES ('Cape Verde','Cape Verdian');
-
 INSERT INTO Country(name, nationality) VALUES ('Central African Republic','Central African');
-
 INSERT INTO Country(name, nationality) VALUES ('Chad','Chadian');
-
 INSERT INTO Country(name, nationality) VALUES ('Chile','Chilean');
-
 INSERT INTO Country(name, nationality) VALUES ('China','Chinese');
-
 INSERT INTO Country(name, nationality) VALUES ('Colombia','Colombian');
-
 INSERT INTO Country(name, nationality) VALUES ('Comoros','Comoran');
-
 INSERT INTO Country(name, nationality) VALUES ('Congo, Republic of the','Congolese');
-
 INSERT INTO Country(name, nationality) VALUES ('Congo, Democratic Republic of the','Congolese');
-
 INSERT INTO Country(name, nationality) VALUES ('Costa Rica','Costa Rican');
-
 INSERT INTO Country(name, nationality) VALUES ('Cote d''Ivoire','Ivorian');
-
 INSERT INTO Country(name, nationality) VALUES ('Croatia','Croatian');
-
 INSERT INTO Country(name, nationality) VALUES ('Cuba','Cuban');
-
 INSERT INTO Country(name, nationality) VALUES ('Cyprus','Cypriot');
-
 INSERT INTO Country(name, nationality) VALUES ('Czech Republic','Czech');
-
 INSERT INTO Country(name, nationality) VALUES ('Denmark','Danish');
-
 INSERT INTO Country(name, nationality) VALUES ('Djibouti','Djibouti');
-
 INSERT INTO Country(name, nationality) VALUES ('Dominica','Dominican');
-
 INSERT INTO Country(name, nationality) VALUES ('Dominican Republic','Dominican');
-
 INSERT INTO Country(name, nationality) VALUES ('East Timor','East Timorese');
-
 INSERT INTO Country(name, nationality) VALUES ('Ecuador','Ecuadorean');
-
 INSERT INTO Country(name, nationality) VALUES ('Egypt','Egyptian');
-
 INSERT INTO Country(name, nationality) VALUES ('El Salvador','Salvadoran');
-
 INSERT INTO Country(name, nationality) VALUES ('Equatorial Guinea','Equatorial Guinean');
-
 INSERT INTO Country(name, nationality) VALUES ('Eritrea','Eritrean');
-
 INSERT INTO Country(name, nationality) VALUES ('Estonia','Estonian');
-
 INSERT INTO Country(name, nationality) VALUES ('Ethiopia','Ethiopian');
-
 INSERT INTO Country(name, nationality) VALUES ('Fiji','Fijian');
-
 INSERT INTO Country(name, nationality) VALUES ('Finland','Finnish');
-
 INSERT INTO Country(name, nationality) VALUES ('France','French');
-
 INSERT INTO Country(name, nationality) VALUES ('Gabon','Gabonese');
-
 INSERT INTO Country(name, nationality) VALUES ('The Gambia','Gambian');
-
 INSERT INTO Country(name, nationality) VALUES ('Georgia','Georgian');
-
 INSERT INTO Country(name, nationality) VALUES ('Germany','German');
-
 INSERT INTO Country(name, nationality) VALUES ('Ghana','Ghanaian');
-
 INSERT INTO Country(name, nationality) VALUES ('Greece','Greek');
-
 INSERT INTO Country(name, nationality) VALUES ('Grenada','Grenadian');
-
 INSERT INTO Country(name, nationality) VALUES ('Guatemala','Guatemalan');
-
 INSERT INTO Country(name, nationality) VALUES ('Guinea','Guinean');
-
 INSERT INTO Country(name, nationality) VALUES ('Guinea-Bissau','Guinea-Bissauan');
-
 INSERT INTO Country(name, nationality) VALUES ('Guyana','Guyanese');
-
 INSERT INTO Country(name, nationality) VALUES ('Haiti','Haitian');
-
 INSERT INTO Country(name, nationality) VALUES ('Honduras','Honduran');
-
 INSERT INTO Country(name, nationality) VALUES ('Hungary','Hungarian');
-
 INSERT INTO Country(name, nationality) VALUES ('Iceland','Icelander');
-
 INSERT INTO Country(name, nationality) VALUES ('India','Indian');
-
 INSERT INTO Country(name, nationality) VALUES ('Indonesia','Indonesian');
-
 INSERT INTO Country(name, nationality) VALUES ('Iran','Iranian');
-
 INSERT INTO Country(name, nationality) VALUES ('Iraq','Iraqi');
-
 INSERT INTO Country(name, nationality) VALUES ('Ireland','Irish');
-
 INSERT INTO Country(name, nationality) VALUES ('Israel','Israeli');
-
 INSERT INTO Country(name, nationality) VALUES ('Italy','Italian');
-
 INSERT INTO Country(name, nationality) VALUES ('Jamaica','Jamaican');
-
 INSERT INTO Country(name, nationality) VALUES ('Japan','Japanese');
-
 INSERT INTO Country(name, nationality) VALUES ('Jordan','Jordanian');
-
 INSERT INTO Country(name, nationality) VALUES ('Kazakhstan','Kazakhstani');
-
 INSERT INTO Country(name, nationality) VALUES ('Kenya','Kenyan');
-
 INSERT INTO Country(name, nationality) VALUES ('Kiribati','I-Kiribati');
 INSERT INTO Country(name, nationality) VALUES ('Korea, North','North Korean');
-
 INSERT INTO Country(name, nationality) VALUES ('Korea, South','South Korean');
-
 INSERT INTO Country(name, nationality) VALUES ('Kuwait','Kuwaiti');
-
 INSERT INTO Country(name, nationality) VALUES ('Kyrgyz Republic','Kirghiz');
-
 INSERT INTO Country(name, nationality) VALUES ('Laos','Laotian');
-
 INSERT INTO Country(name, nationality) VALUES ('Latvia','Latvian');
-
 INSERT INTO Country(name, nationality) VALUES ('Lebanon','Lebanese');
-
 INSERT INTO Country(name, nationality) VALUES ('Lesotho','Mosotho');
-
 INSERT INTO Country(name, nationality) VALUES ('Liberia','Liberian');
-
 INSERT INTO Country(name, nationality) VALUES ('Libya','Libyan');
-
 INSERT INTO Country(name, nationality) VALUES ('Liechtenstein','Liechtensteiner');
-
 INSERT INTO Country(name, nationality) VALUES ('Lithuania','Lithuanian');
-
 INSERT INTO Country(name, nationality) VALUES ('Luxembourg','Luxembourger');
-
 INSERT INTO Country(name, nationality) VALUES ('Macedonia','Macedonian');
-
 INSERT INTO Country(name, nationality) VALUES ('Madagascar','Malagasy');
-
 INSERT INTO Country(name, nationality) VALUES ('Malawi','Malawian');
-
 INSERT INTO Country(name, nationality) VALUES ('Malaysia','Malaysian');
-
 INSERT INTO Country(name, nationality) VALUES ('Maldives','Maldivan');
-
 INSERT INTO Country(name, nationality) VALUES ('Mali','Malian');
-
 INSERT INTO Country(name, nationality) VALUES ('Malta','Maltese');
-
 INSERT INTO Country(name, nationality) VALUES ('Marshall Islands','Marshallese');
-
 INSERT INTO Country(name, nationality) VALUES ('Mauritania','Mauritanian');
-
 INSERT INTO Country(name, nationality) VALUES ('Mauritius','Mauritian');
-
 INSERT INTO Country(name, nationality) VALUES ('Mexico','Mexican');
-
 INSERT INTO Country(name, nationality) VALUES ('Federated States of Micronesia','Micronesian');
-
 INSERT INTO Country(name, nationality) VALUES ('Moldova','Moldovan');
-
 INSERT INTO Country(name, nationality) VALUES ('Monaco','Monegasque');
-
 INSERT INTO Country(name, nationality) VALUES ('Mongolia','Mongolian');
-
 INSERT INTO Country(name, nationality) VALUES ('Morocco','Moroccan');
-
 INSERT INTO Country(name, nationality) VALUES ('Mozambique','Mozambican');
-
-INSERT INTO Country(name, nationality) VALUES ('Myanmar (Burma);','Burmese');
-
+INSERT INTO Country(name, nationality) VALUES ('Myanmar (Burma)','Burmese');
 INSERT INTO Country(name, nationality) VALUES ('Namibia','Namibian');
-
 INSERT INTO Country(name, nationality) VALUES ('Nauru','Nauruan');
-
 INSERT INTO Country(name, nationality) VALUES ('Nepal','Nepalese');
-
 INSERT INTO Country(name, nationality) VALUES ('Netherlands','Dutch');
-
 INSERT INTO Country(name, nationality) VALUES ('New Zealand','New Zealander');
-
 INSERT INTO Country(name, nationality) VALUES ('Nicaragua','Nicaraguan');
-
 INSERT INTO Country(name, nationality) VALUES ('Niger','Nigerien');
-
 INSERT INTO Country(name, nationality) VALUES ('Nigeria','Nigerian');
-
 INSERT INTO Country(name, nationality) VALUES ('Norway','Norwegian');
-
 INSERT INTO Country(name, nationality) VALUES ('Oman','Omani');
-
 INSERT INTO Country(name, nationality) VALUES ('Pakistan','Pakistani');
-
 INSERT INTO Country(name, nationality) VALUES ('Palau','Palauan');
-
 INSERT INTO Country(name, nationality) VALUES ('Panama','Panamanian');
-
 INSERT INTO Country(name, nationality) VALUES ('Papua New Guinea','Papua New Guinean');
-
 INSERT INTO Country(name, nationality) VALUES ('Paraguay','Paraguayan');
-
 INSERT INTO Country(name, nationality) VALUES ('Peru','Peruvian');
-
 INSERT INTO Country(name, nationality) VALUES ('Philippines','Filipino');
-
 INSERT INTO Country(name, nationality) VALUES ('Poland','Polish');
-
 INSERT INTO Country(name, nationality) VALUES ('Portugal','Portuguese');
-
 INSERT INTO Country(name, nationality) VALUES ('Qatar','Qatari');
-
 INSERT INTO Country(name, nationality) VALUES ('Romania','Romanian');
-
 INSERT INTO Country(name, nationality) VALUES ('Russia','Russian');
-
 INSERT INTO Country(name, nationality) VALUES ('Rwanda','Rwandan');
-
 INSERT INTO Country(name, nationality) VALUES ('Saint Kitts and Nevis','Kittian and Nevisian');
-
 INSERT INTO Country(name, nationality) VALUES ('Saint Lucia','Saint Lucian');
-
 INSERT INTO Country(name, nationality) VALUES ('Samoa','Samoan');
-
 INSERT INTO Country(name, nationality) VALUES ('San Marino','Sammarinese');
-
 INSERT INTO Country(name, nationality) VALUES ('Sao Tome and Principe','Sao Tomean');
-
 INSERT INTO Country(name, nationality) VALUES ('Saudi Arabia','Saudi Arabian');
-
 INSERT INTO Country(name, nationality) VALUES ('Senegal','Senegalese');
-
 INSERT INTO Country(name, nationality) VALUES ('Serbia and Montenegro','Serbian');
-
 INSERT INTO Country(name, nationality) VALUES ('Seychelles','Seychellois');
-
 INSERT INTO Country(name, nationality) VALUES ('Sierra Leone','Sierra Leonean');
-
 INSERT INTO Country(name, nationality) VALUES ('Singapore','Singaporean');
-
 INSERT INTO Country(name, nationality) VALUES ('Slovakia','Slovak');
-
 INSERT INTO Country(name, nationality) VALUES ('Slovenia','Slovene');
-
 INSERT INTO Country(name, nationality) VALUES ('Solomon Islands','Solomon Islander');
-
 INSERT INTO Country(name, nationality) VALUES ('Somalia','Somali');
-
 INSERT INTO Country(name, nationality) VALUES ('South Africa','South African');
-
 INSERT INTO Country(name, nationality) VALUES ('Spain','Spanish');
-
 INSERT INTO Country(name, nationality) VALUES ('Sri Lanka','Sri Lankan');
-
 INSERT INTO Country(name, nationality) VALUES ('Sudan','Sudanese');
-
 INSERT INTO Country(name, nationality) VALUES ('Suriname','Surinamer');
-
 INSERT INTO Country(name, nationality) VALUES ('Swaziland','Swazi');
-
 INSERT INTO Country(name, nationality) VALUES ('Sweden','Swedish');
-
 INSERT INTO Country(name, nationality) VALUES ('Switzerland','Swiss');
-
 INSERT INTO Country(name, nationality) VALUES ('Syria','Syrian');
-
 INSERT INTO Country(name, nationality) VALUES ('Taiwan','Taiwanese');
-
 INSERT INTO Country(name, nationality) VALUES ('Tajikistan','Tadzhik');
-
 INSERT INTO Country(name, nationality) VALUES ('Tanzania','Tanzanian');
-
 INSERT INTO Country(name, nationality) VALUES ('Thailand','Thai');
-
 INSERT INTO Country(name, nationality) VALUES ('Togo','Togolese');
-
 INSERT INTO Country(name, nationality) VALUES ('Tonga','Tongan');
-
 INSERT INTO Country(name, nationality) VALUES ('Trinidad and Tobago','Trinidadian');
-
 INSERT INTO Country(name, nationality) VALUES ('Tunisia','Tunisian');
-
 INSERT INTO Country(name, nationality) VALUES ('Turkey','Turkish');
-
 INSERT INTO Country(name, nationality) VALUES ('Turkmenistan','Turkmen');
-
 INSERT INTO Country(name, nationality) VALUES ('Tuvalu','Tuvaluan');
-
 INSERT INTO Country(name, nationality) VALUES ('Uganda','Ugandan');
-
 INSERT INTO Country(name, nationality) VALUES ('Ukraine','Ukrainian');
-
 INSERT INTO Country(name, nationality) VALUES ('United Arab Emirates','Emirian');
-
 INSERT INTO Country(name, nationality) VALUES ('United Kingdom','British');
-
 INSERT INTO Country(name, nationality) VALUES ('United States','American');
-
 INSERT INTO Country(name, nationality) VALUES ('Uruguay','Uruguayan');
-
 INSERT INTO Country(name, nationality) VALUES ('Uzbekistan','Uzbekistani');
-
 INSERT INTO Country(name, nationality) VALUES ('Vanuatu','Ni-Vanuatu');
-
-INSERT INTO Country(name, nationality) VALUES ('Vatican City (Holy See);','none');
-
+INSERT INTO Country(name, nationality) VALUES ('Vatican City (Holy See)','none');
 INSERT INTO Country(name, nationality) VALUES ('Venezuela','Venezuelan');
-
 INSERT INTO Country(name, nationality) VALUES ('Vietnam','Vietnamese');
-
 INSERT INTO Country(name, nationality) VALUES ('Yemen','Yemeni');
-
 INSERT INTO Country(name, nationality) VALUES ('Zambia','Zambian');
-
 INSERT INTO Country(name, nationality) VALUES ('Zimbabwe','Zimbabwean');
