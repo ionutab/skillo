ecbaccfee3cb1041c17dca0d3af22f8002dbb14a
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-09-22 00:58:01 +0300

    so, using correct classes now

---
+++ src/groovy/skillo/filters/CandidateFilter.groovy
@@ -0,0 +1,12 @@
+package skillo.filters
+
+/**
+ * Created by ionutab on 9/13/2014.
+ */
+class CandidateFilter extends BasicSearchFilter implements Serializable {
+
+    protected List<String> qualifications
+
+
+
+}
