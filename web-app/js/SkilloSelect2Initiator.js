204f23ff312c7e2142597b602e3648acbe697754
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-10-01 21:02:53 +0300

    added little piece of code to preselect values

--- web-app/js/SkilloSelect2Initiator.js
+++ web-app/js/SkilloSelect2Initiator.js
@@ -18,7 +18,6 @@ var SkilloSelect2QualificationsFormatting = (function(){
     };
 
 })();
-
 var SkilloSelect2MultipleQulificationInitiator = (function(){
 
     var doWeHaveAQualificationAlready = "Select one or more qualifications";
@@ -40,13 +39,26 @@ var SkilloSelect2MultipleQulificationInitiator = (function(){
             multiple: true,
             ajax:{
                 url: ajaxURL,
-                dataType: 'json',
                 data: function(term, page){
                     return {inputCode: term};
                 },
                 results: function (data, page) {
                     return {results: data};
                 }
+            },
+            initSelection: function(element, callback) {
+                var data = [{ id: 'user0', name: 'Disabled User'}, { id: 'user1', name: 'Jane Doe'}];
+                /*
+                    $(element.val().split(",")).each(function(i) {
+                        var item = this.split(':');
+                        data.push({
+                            id: item[0],
+                            name: item[1]
+                        });
+                    });
+                */
+                //$(element).val('');
+                callback(data);
             },
             formatSelection: SkilloSelect2QualificationsFormatting.formatQualificationSelection,
             formatResult: SkilloSelect2QualificationsFormatting.formatQualificationResult,
