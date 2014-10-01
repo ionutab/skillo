25fae98639b68d0f575f1cf10106395fa413d205
Alexandru Ionut Bujdei <bujdeialexandru@gmail.com>
2014-10-01 21:04:55 +0300

    modified qualifications. added complicated toString()

--- src/groovy/skillo/filters/CandidateFilter.groovy
+++ src/groovy/skillo/filters/CandidateFilter.groovy
@@ -1,12 +1,30 @@
 package skillo.filters
 
-/**
- * Created by ionutab on 9/13/2014.
- */
 class CandidateFilter extends BasicSearchFilter implements Serializable {
 
-    protected List<String> qualifications
+    protected Collection<Collection<Long>> qualifications
 
+    Collection<Collection<Long>> getQualifications() {
+        return qualifications
+    }
 
+    void setQualifications(Collection<Collection<Long>> qualifications) {
+        this.qualifications = qualifications
+    }
 
+    @Override
+    public String toString() {
+        String qualificationsString = new String("\n")
+        for(Collection<Long> c : qualifications){
+
+            for(Long l : c){
+                qualificationsString += l + ","
+            }
+            qualificationsString += "\n"
+        }
+
+        return "CandidateFilter{" +
+                "qualifications=" + qualifications.size() + ":"+ qualificationsString +
+                '}';
+    }
 }
